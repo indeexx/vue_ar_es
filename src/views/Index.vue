d1802573d58aeb361e0b925dde415e45d30db7f3
indeex <indeex@qq.com>
2018-07-10 00:37:24 +0800

    init

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -22,7 +22,7 @@ export default class Index extends Vue {
     this.init();
     window.addEventListener('resize', () => {
       this.engine.resize();
-    })
+    });
   }
   private init(): void {
     console.log("init...");
@@ -30,49 +30,60 @@ export default class Index extends Vue {
     this.canvas = document.getElementById('renderCanvas');
     this.engine = new INDEEX.Engine(this.canvas);
     this.scene = new INDEEX.Scene(this.engine);
+    this.scene.clearColor = new BABYLON.Color4(0, 0, 0.1, 0.1);
     this.engine.runRenderLoop(() => {
       that.scene.render();
     });
-    this.camera = new INDEEX.FreeCamera('freeCamera1', new INDEEX.Vector3(10,20,30), this.scene);
+    this.camera = new INDEEX.FreeCamera('freeCamera1', new INDEEX.Vector3(4, 4, -4), this.scene);
     this.camera.setTarget(INDEEX.Vector3.Zero());
     this.camera.attachControl(this.canvas, false);
+    this.camera.position.x = 3;
+    this.camera.position.y = 5;
+    this.camera.position.z = -3;
     this.pointLight = new INDEEX.PointLight('ponitLigth1', INDEEX.Vector3.Zero(), this.scene);
-    this.pointLight.position = new INDEEX.Vector3(20, 20, 20);
-    this.pointLight.diffuse = new INDEEX.Color3(0, 1, 0);//灯光固定颜色 绿色
-    this.pointLight.specular = new INDEEX.Color3(1, 0, 0);//漫反射 红色
+    this.pointLight.position = new INDEEX.Vector3(10, 2, 6);
+    this.pointLight.diffuse = new INDEEX.Color3(1, 1, 1);//灯光固定颜色 绿色
+    this.pointLight.specular = new INDEEX.Color3(1, 1, 1);//漫反射 红色
     this.pointLight.intensity = 1.0;//光照强度 默认1.0
-    this.light = new INDEEX.HemisphericLight('light1', new INDEEX.Vector3(0,1,0), this.scene);
-    let sphere = INDEEX.MeshBuilder.CreateSphere('sphere', {segments:16, diameter:2}, this.scene);
-    sphere.position.y = 1;
-    var ground = INDEEX.MeshBuilder.CreateGround('ground1', {height:6, width:6, subdivisions: 2}, this.scene);
-    INDEEX.SceneLoader.Load( 'assets/models/babylon/test/girl01/', 'girl01.babylon', this.engine, (scene: any) => {
-      //add orther scene
-      console.log('load success');
-      scene.executeWhenReady(function () {
-        scene.activeCamera.attachControl(that.canvas);
-        that.engine.runRenderLoop(function() {
-          scene.render();
-        });
-      });
-    }, (progress: any) => {
-      //progress
-      console.log('progress');
-    }, (error: any) => {
-      // error
-      console.log('error');
-    });
-    // INDEEX.SceneLoader.ImportMeshAsync('', '../assets/models/', 'test2.babylon', this.scene);
+    this.light = new INDEEX.HemisphericLight('light1', new INDEEX.Vector3(2,1,2), this.scene);
+    // let sphere = INDEEX.MeshBuilder.CreateSphere('sphere', {segments:16, diameter:2}, this.scene);
+    // sphere.position.y = 1;
+    var ground = INDEEX.MeshBuilder.CreateGround('ground1', {height:5, width:5, subdivisions: 1}, this.scene);
+    ground.position = new BABYLON.Vector3(0, -1, 0);
 
+    // INDEEX.SceneLoader.Load( 'assets/models/babylon/test/girl01/', 'girl01.babylon', this.engine, (scene: any) => {
+    //   //add orther scene
+    //   console.log('load success');
+    //   scene.executeWhenReady(function () {
+    //     scene.activeCamera.attachControl(that.canvas);
+    //     that.engine.runRenderLoop(function() {
+    //       scene.render();
+    //     });
+    //   });
+    // }, (progress: any) => {
+    //   //progress
+    //   console.log('progress');
+    // }, (error: any) => {
+    //   // error
+    //   console.log('error');
+    // });
+    let girl:any = INDEEX.SceneLoader.ImportMeshAsync('', 'assets/models/babylon/test/girl01/', 'girl01.babylon', this.scene, (progress: any) => {
+      let total: number = progress.total;
+      let loaded: number = progress.loaded;
+      if(total == loaded){
+        console.log('load sucess.')
+      }
+    });
 
     //skybox
-    let skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, this.scene);
-    let skyboxMaterial = new BABYLON.StandardMaterial("skyBox", this.scene);
-    skyboxMaterial.backFaceCulling = false;
-    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("assets/models/skybox/skybox/", this.scene);
-    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
-    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
-    skybox.material = skyboxMaterial;
+    // let skybox = INDEEX.MeshBuilder.CreateBox("skyBox", {size:1000.0}, this.scene);
+    // let skyboxMaterial = new INDEEX.StandardMaterial("skyBox", this.scene);
+    // skyboxMaterial.backFaceCulling = false;
+    // skyboxMaterial.reflectionTexture = new INDEEX.CubeTexture("assets/models/skybox/skybox/", this.scene);
+    // skyboxMaterial.reflectionTexture.coordinatesMode = INDEEX.Texture.SKYBOX_MODE;
+    // skyboxMaterial.diffuseColor = new INDEEX.Color3(0, 0, 0);
+    // skyboxMaterial.specularColor = new INDEEX.Color3(0, 0, 0);
+    // skybox.material = skyboxMaterial;
   }
 }
 </script>
@@ -81,6 +92,7 @@ export default class Index extends Vue {
 .container {
   width: 100%;
   height: 100%;
+  overflow: hidden;
   #renderCanvas {
     width: 100%;
     height: 100%;
